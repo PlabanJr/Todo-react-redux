d7683d3f171afe268e4c77622b9b4bf569b1b0e8
Plaban J Rajkhowa <plabanj@geekyants.com>
2019-12-07 14:32:47 +0530

    feat: adds Screenshots & fix typos

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -3,35 +3,54 @@ import { connect } from "react-redux";
 import { showAll, showDone, showIncomplete } from "../actions/action";
 
 class Filters extends Component {
+  state = { 
+    activeIndex: 0
+  }
+
   render() {
+    const {activeIndex} = this.state
+
     return (
       <div className="details">
         <div className="details-wrapper">
           <div
             className="total-items filter-btn"
-            onClick={() => this.props.filterAll()}
-            style={{
-              width: 40,
-              borderTopLeftRadius: 5,
-              borderBottomLeftRadius: 5
+            onClick={() => {
+              this.setState({activeIndex: 0})
+              this.props.filterAll()
             }}
+            style={
+                {
+                  width: 40,
+                  borderTopLeftRadius: 5,
+                  borderBottomLeftRadius: 5,
+                  backgroundColor: activeIndex === 0 ? "#ddd" : null
+                }
+            }
           >
             All
           </div>
 
           <div
             className="done-items filter-btn"
-            onClick={() => this.props.filterDone()}
+            onClick={() => {
+              this.setState({activeIndex: 1})
+              this.props.filterDone()
+            }}
+            style={{ backgroundColor: activeIndex === 1 ? "#ddd" : null }}
           >
             Completed
           </div>
 
           <div
             className="undone-items filter-btn"
-            onClick={() => this.props.filterIncomplete()}
-            style={{ borderTopRightRadius: 5, borderBottomRightRadius: 5 }}
+            onClick={() => {
+              this.setState({activeIndex: 2})
+              this.props.filterIncomplete()
+            }}
+            style={{ borderTopRightRadius: 5, borderBottomRightRadius: 5, backgroundColor: activeIndex === 2 ? "#ddd" : null }}
           >
-            Incompleted
+            Incomplete
           </div>
         </div>
       </div>
